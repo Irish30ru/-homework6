37f9cbc21a69d9cdaa89cb889f109fc80e220be0
Andrey <akokinn2003@yandex.ru>
2023-01-30 11:47:47 +0400

    Task1 complete

--- homework6.cs
+++ homework6.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Find_numbers_more_zero(int count)
+{
+
+    string x = "";
+    int counter = 0;
+
+    int[] array = new int[count];
+    Console.WriteLine("Введите числа: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        array[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+    
+        if (array[i]>0)
+        {
+            counter+=1;
+        }
+        
+
+    }
+    x = string.Join(", ", array);
+
+
+    
+    return counter;
+
+}
+Console.Write("Введите колличество чисел: ");
+int result = Convert.ToInt32(Console.ReadLine());
+Console.Write(Find_numbers_more_zero(result));
